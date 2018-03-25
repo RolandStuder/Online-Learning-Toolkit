ce349fdd2af206bb07667fccc2747976200ccf03
Roland Studer <roland.studer@gmail.com>
2018-03-25 11:48:40 +0200

    correcting host-url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ App::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'online-learning-toolkit.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'online-learning-toolkit.herokuapp.com' }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
